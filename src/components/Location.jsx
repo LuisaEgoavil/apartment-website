b146f5bb6b6acc6d082135f8f072ca7aad94a07c
Luisa Egoavil <lupe.egoavil@gmail.com>
2023-02-14 22:54:59 +0100

    add styles for location page

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,17 +1,17 @@
+import './Location.scss'
+
 function Location () {
   return (
-    <>
-    <iframe 
+    <div className='google-map'>
+      <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2493.7859278207316!2d9.474133715531705!3d51.315062679603905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bb3f443ccfd7b5%3A0x9b373220b3231d1a!2sGoethestra%C3%9Fe%206%2C%2034119%20Kassel!5e0!3m2!1sde!2sde!4v1676410025290!5m2!1sde!2sde" 
-        width="600" 
-        height="450" 
         style={{order:0}}
         allowfullscreen="" 
         loading="lazy" 
         referrerpolicy="no-referrer-when-downgrade"
       >
       </iframe>
-    </>
+    </div>
   )
 }
 
