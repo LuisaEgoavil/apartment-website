9d7449f82d1f522211d424d340881842c7df7289
Luisa Egoavil <lupe.egoavil@gmail.com>
2023-02-15 23:30:14 +0100

    add phone number

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,7 @@ function Contact() {
     <div>
       {/* TODO: change the number */}
       <a href="https://wa.me/+4917653832422" target="_blank"className='whatsapp-logo'>Contáctame por Whatsapp</a> 
+      <a href="tel:+4917653832422">+4917653832422</a>
     </div>
   )
 }
