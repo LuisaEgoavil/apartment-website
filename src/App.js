f9f0f0c66358211eb567e6301aae55ced843ab75
Luisa Egoavil <lupe.egoavil@gmail.com>
2023-02-19 20:04:00 +0100

    add notfound page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route , Navigate } from "react-router-dom";
 import './App.scss';
 import Navbar from "./components/Navbar"
 import Location from "./components/Location";
 import Gallery from "./components/Gallery";
 import Contact from "./components/Contact";
-import Home from "./components/Home"
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/location" element={<Location />}></Route>
         <Route path="/gallery" element={<Gallery />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,16 @@
+import Fade from 'react-reveal/Fade';
+
+function Contact() {
+
+  return (
+    <div>
+      <Fade>
+        {/* TODO: add seconds for timeout */}
+        {/* <p>oops this page doesn't exist</p>
+        <h1>Redirecting in 5 seconds...</h1> */}
+      </Fade>
+    </div>
+  )
+}
+
+export default Contact
