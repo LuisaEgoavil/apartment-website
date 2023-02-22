c14397ac8f58b91f466a55ce78406412c0df3f1a
Luisa Egoavil <lupe.egoavil@gmail.com>
2023-02-22 23:49:10 +0100

    add styles and icons for contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,7 @@ import './Contact.scss';
 import { useTranslation } from 'react-i18next';
 import Fade from 'react-reveal/Fade';
 import {Helmet} from "react-helmet";
+import { BsWhatsapp, BsTelephone } from "react-icons/bs";
 
 function Contact() {
   const { t } = useTranslation();
@@ -13,17 +14,19 @@ function Contact() {
       </Helmet>
       <Fade>
       {/* TODO: change the number */}
-        <p>
+       <div>
+          <BsWhatsapp />
           <a 
-            href="https://wa.me/+4917653832422" 
+            href="https://wa.me/+51952060910" 
             target="_blank"
             rel="noreferrer"
             className='whatsapp-logo'
-            >{t('contact.whatsapp_number')}</a> 
-        </p>
-        <p>
-          <a href="tel:+4917653832422" target="_blank" rel="noreferrer">{t('contact.phone_number')}</a>
-        </p>
+          >{t('contact.whatsapp_number')}</a> 
+        </div>
+        <div>
+          <BsTelephone />
+          <a href="tel:+51952060910" target="_blank" rel="noreferrer">{t('contact.phone_number')}</a>
+        </div>
       </Fade>
     </div>
   )
